1409228706be286a1b851f44809db236aadbf13a
SNOMZZZ <sonodarei@outlook.jp>
2022-04-15 13:38:24 +0900

    practice

--- src/dwa.cpp
+++ src/dwa.cpp
@@ -1,6 +1,12 @@
 #include<stdio.h>
 
 DWA::DWA():private_nh("~"){
-    private_nh.param("hz", hz, {10});
-    
+    private_nh.getParam("hz", hz);                                  //looprate
+    private_nh.getParam("roomba_radius", roomba_radius);            //turn radius
+    private_nh.getParam("max_speed", max_speed);                    //max speed of roomba
+    private_nh.getParam("min_speed", min_speed);                    //minimum speed of roomba
+    private_nh.getParam("max_yawrate", max_yawrate);                //turn speed of roomba
+    private_nh.getParam("max_accel", max_accel);
+
+
 }
